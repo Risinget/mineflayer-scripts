ee35aaaa439a6761ed2dd9cbff98114dc44a61fb
Risinget <tenorioquirozeduar7@gmail.com>
2024-03-14 11:46:29 -0400

    time elapsed

--- goToAPlaceEating.js
+++ goToAPlaceEating.js
@@ -4,6 +4,8 @@ const {
   Movements,
   goals: { GoalBlock },
 } = require("mineflayer-pathfinder");
+const moment = require('moment'); // Asegúrate de tener 'moment' instalado para manejar fechas y horas
+
 
 const bot = mineflayer.createBot({
   host: "172.233.188.131", // Servidor de Minecraft
@@ -26,7 +28,7 @@ bot.on('health', ()=>{
     console.log('mI cOMIDA: '+bot.food);
 
 
- if (bot.food < 20)
+ if (bot.food < 14)
  {
    eatFood()
      .then(() => {
@@ -49,11 +51,15 @@ bot.on("chat", (username, message) => {
   if (message.startsWith("goto ")) {
     const [_, x, y, z] = message.split(" ").map((v) => parseInt(v, 10));
     lastGoal = new GoalBlock(x, y, z); // Almacena el objetivo actual antes de moverse
+
+    // Inicia el seguimiento del tiempo
+    startTime = Date.now();
     bot.pathfinder.setGoal(lastGoal);
     checkHunger(); // Comienza la comprobación de hambre
   }
 });
 
+// Modificar la función eatFood para imprimir el mensaje cada vez que se invoca
 async function eatFood() {
   const foodItem = bot.inventory.items().find((item) => item.name.includes('enchanted_golden_apple'));
   if (foodItem) {
@@ -70,13 +76,16 @@ async function eatFood() {
       // Restablece el último objetivo almacenado para que el bot continúe su camino
       if (lastGoal) {
         bot.pathfinder.setGoal(lastGoal);
+        // Si startTime es null, significa que el bot no está en camino a un objetivo
+        if (startTime === null) {
+          startTime = Date.now(); // Reinicia el contador de tiempo
+        }
       }
     } catch (err) {
       console.log(`Error al comer: ${err.message}`);
     }
   }
 }
-
 function checkHunger() {
   if (
     bot.food < 20)
@@ -93,3 +102,15 @@ function checkHunger() {
     setTimeout(checkHunger, 1000); // Sigue revisando el hambre cada segundo
   }
 }
+
+bot.on("goal_reached", (goal) => {
+  const totalTime = (Date.now() - startTime) / 1000; // Tiempo total en segundos
+  console.log(
+    `[${moment().format("HH:mm:ss")}] ${goal.x}, ${goal.y}, ${
+      goal.z
+    } Tiempo Total: ${totalTime.toFixed(2)} segundos`
+  );
+  startTime = null; // Resetea el tiempo de inicio para el próximo objetivo
+});
+
+
