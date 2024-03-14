0347cc1b655b6e1a1d2829843166cf39cb928caf
Risinget <tenorioquirozeduar7@gmail.com>
2024-03-14 08:54:07 -0400

    added AntiAfk.js

---
+++ antiAFK.js
@@ -0,0 +1,33 @@
+const mineflayer = require("mineflayer");
+
+// Configura aquí los detalles de tu bot
+const bot = mineflayer.createBot({
+  host: "172.233.188.131",
+  port: 49999,
+  username: "username1234",
+});
+
+// El bot ha entrado al servidor
+bot.once("spawn", () => {
+  console.log("El bot ha entrado al servidor.");
+
+  // Función para realizar una acción simple y evitar el AFK
+  const antiAFK = () => {
+    // Hace que el bot rote ligeramente para evitar ser detectado como AFK
+    let yaw = bot.entity.yaw + Math.PI / 4;
+    let pitch = bot.entity.pitch + Math.PI / 4;
+    bot.look(yaw, pitch, false);
+
+    // O podrías hacer que el bot se mueva ligeramente
+    // bot.setControlState('forward', true);
+    // setTimeout(() => bot.setControlState('forward', false), 1000);
+
+    console.log("Realizando acción anti-AFK.");
+  };
+
+  // Ejecutar la función antiAFK cada 10 minutos
+  setInterval(antiAFK, 1000 * 10 * 1);
+});
+
+// Manejo de errores
+bot.on("error", (err) => console.log(err));
